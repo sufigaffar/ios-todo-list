1c76b05eefaed665eed7b885d3c7ef80a00f3213
Sufi Gaffar <sufi@sufigaffar.com>
2020-12-29 18:19:50 +0000

    chore: initial setup

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sufi Gaffar on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
